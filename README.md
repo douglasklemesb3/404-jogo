8973f35e2c1d07b6260204f9f5f4c15a36f781cb
douglas <douglasklemes0303@gmail.com>
2019-07-25 17:46:42 -0300

    Adicionar jogo

---
+++ README.md
@@ -0,0 +1,29 @@
+# JOGO DAS CORES
+
+## linguagens utilizadas
+- html
+- css
+- javascript
+---
+### ferramentos das linguagens
+
+- Utilizamos em `CSS` display flex e propriendades simples de ediçao
+- em `Javascript` ,utilizamos `FOR` ,`MATH`,`FUNCTION`.
+
+---
+
+**EX: funçao resetar jogo**
+~~~
+
+
+
+
+
+
+
+
+~~~
+
+ficou com alguma dúvida?? [clique aqui](http://google.com.br)
+
+**Confie em mim** Quer Jogar?? [clique aqui]()
---
+++ script.js
@@ -0,0 +1,20 @@
+/*let numero = Math.round(2.4);  //arredonda para o mais proximo
+
+numero =Math.floor(5.5);  //arredonda para baixo 
+
+numero = Math.ceil(12.1); //arredonda para cima 
+
+numero = Math.pow(2,3); // expoente
+
+numero = Math.sqrt(25); // rais quadrada
+
+numero = Math.PI;
+
+numero = Math.sin(0*Math.pi /4); //sin seno ou cos coseno
+
+console.log(numero);*/
+
+/*let numero = Math.round(Math.random() *6); //gerar numeros aleatorios arredondando para cima
+
+console.log(numero);*/
+
